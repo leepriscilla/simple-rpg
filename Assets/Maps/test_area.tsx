1a9980c5507fddc3f5c9449a22af78ab15913055
leepriscilla <lee.priscilla98@gmail.com>
2022-01-21 23:38:18 -0800

    making tile maps

---
+++ Assets/Maps/test_area.tsx
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="test_area" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
+ <image source="../Art/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="60">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="61">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="116">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="117">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="118">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="526">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="643">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <wangsets>
+  <wangset name="Water" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="2" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="3" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="4" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="59" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="60" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="61" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="116" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="117" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="118" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+  <wangset name="Unnamed Set" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="520" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="521" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="522" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="577" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="578" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="579" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="634" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="635" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="636" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
