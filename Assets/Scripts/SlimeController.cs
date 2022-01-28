9e8a5e201b162f31a71f843548a10364506bcd0e
leepriscilla <lee.priscilla98@gmail.com>
2022-01-27 23:06:14 -0800

    player killing & enemy movement

--- Assets/Scripts/SlimeController.cs
+++ Assets/Scripts/SlimeController.cs
@@ -1,26 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SlimeController : MonoBehaviour
 {
     public float moveSpeed;
     public float timeBetweenMove;
     public float timeToMove;
+    public float waitToReload;
 
     private Rigidbody2D myRigidbody;
     private bool moving;
     private Vector3 moveDirection;
     private float timeBetweenMoveCounter;
     private float timeToMoveCounter;
+    private bool reloading;
+    private GameObject thePlayer;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
 
-        timeBetweenMoveCounter = timeBetweenMove;
-        timeToMoveCounter = timeToMove;
+        //timeBetweenMoveCounter = timeBetweenMove;
+        //timeToMoveCounter = timeToMove;
+        timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
+        timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeToMove * 1.25f);
     }
 
     // Update is called once per frame
@@ -34,7 +40,8 @@ public class SlimeController : MonoBehaviour
             if(timeToMoveCounter < 0f)
             {
                 moving = false;
-                timeBetweenMoveCounter = timeBetweenMove;
+                //timeBetweenMoveCounter = timeBetweenMove;
+                timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
             }
         }
         else
@@ -45,10 +52,29 @@ public class SlimeController : MonoBehaviour
             if(timeBetweenMoveCounter < 0f)
             {
                 moving = true;
-                timeToMoveCounter = timeToMove;
-                
+                //timeToMoveCounter = timeToMove;
+                timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeToMove * 1.25f);
                 moveDirection = new Vector3(Random.Range(-1f,1f) * moveSpeed, Random.Range(-1f,1f) * moveSpeed, 0f);
             }
+        }
+        if(reloading)
+        {
+            waitToReload -= Time.deltaTime;
+            if(waitToReload < 0f){
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                thePlayer.SetActive(true);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //If slime collides with Player, set Player to inactive and reload the scene
+        if(other.gameObject.name == "Player")
+        {
+            other.gameObject.SetActive(false);
+            reloading = true;
+            thePlayer = other.gameObject;
         }
     }
 }
