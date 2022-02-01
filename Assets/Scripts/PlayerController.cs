2c463c555413f087f84ef286744a5e073f2d40b2
leepriscilla <lee.priscilla98@gmail.com>
2022-01-31 17:48:55 -0800

    diagonal movement speed & animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public Vector2 lastMove;
     public float attackTime;
     public string startPoint;
+    public float diagonalMoveModifier;
 
     private Animator anim;
     private Rigidbody2D myRigidbody;
@@ -15,6 +16,7 @@ public class PlayerController : MonoBehaviour
     private static bool playerExists;
     private bool attacking;
     private float attackTimeCounter;
+    private float currentMoveSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -45,7 +47,7 @@ public class PlayerController : MonoBehaviour
             if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
             {
                 //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
-                myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidbody.velocity.y);
+                myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * currentMoveSpeed, myRigidbody.velocity.y);
                 playerMoving = true;
                 lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
             }
@@ -54,7 +56,7 @@ public class PlayerController : MonoBehaviour
             if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
             {
                 //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);            
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * currentMoveSpeed);            
                 playerMoving = true;
                 lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
             }
@@ -75,6 +77,15 @@ public class PlayerController : MonoBehaviour
                 attacking = true;
                 myRigidbody.velocity = Vector2.zero; //create Vector2 with value, (0,0)
                 anim.SetBool("Attack", true);
+            }
+
+            if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f && Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.5f)
+            {
+                currentMoveSpeed = moveSpeed * diagonalMoveModifier;
+            }
+            else
+            {
+                currentMoveSpeed = moveSpeed;
             }
         }
 
